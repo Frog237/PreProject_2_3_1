9b336702b360cf4058cad0c8d0a07f0455ec7772
Frog237 <yt86tu@mail.ru>
2025-03-14 17:53:59 +0300

    Убрал транзакции из dao

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -15,31 +15,26 @@ public class UserDaoImp implements UserDao {
     private EntityManager entityManager;
 
     @Override
-    @Transactional
     public void add(User user) {
         entityManager.persist(user);
     }
 
     @Override
-    @Transactional
     public User findById(Long id) {
         return entityManager.find(User.class, id);
     }
 
     @Override
-    @Transactional
     public List<User> findAll() {
         return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
     }
 
     @Override
-    @Transactional
     public void update(User user) {
         entityManager.merge(user);
     }
 
     @Override
-    @Transactional
     public void delete(Long id) {
         User user = findById(id);
         if (user != null) {
